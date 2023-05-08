131eecf40b89300e829794f80620a4fa80ea391b
Hasnat Shafin <sh.shafin20@gmail.com>
2023-05-09 03:02:31 +0600

    initial commit

--- TestYourLogicalThinking1.cpp
+++ TestYourLogicalThinking1.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 int main()
 {
-    int t,count=0;
+    int t;
     cin>>t;
     while(t--)
     {
-        int n;
+        int n,count=0;
         cin>>n;
         int num[n];
         for(int i=0;i<n;i++)
         {
             cin>>num[i];
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(num[i]=='1' ||num[i]=='2' ||num[i]=='3' ||num[i]=='4' ||num[i]=='5' ||num[i]=='6' || num[i]=='7'){count++;} 
-            if(count==7){cout<<i<<endl;}
+            if(num[i]>=1 && num[i]<=7){
+                count++;
+                if(count==7){
+                cout<<i+1<<endl;}
+            }
         }
     }
     return 0;
