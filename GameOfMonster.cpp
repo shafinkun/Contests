dc44e6c124ff192bf248139fd9e64c164b48a1c7
Hasnat Shafin <sh.shafin20@gmail.com>
2023-05-12 22:40:37 +0600

    initial commit

---
+++ GameOfMonster.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, spell1, count1 = 0;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            if (a[i] == 1)
+            {
+                count1++;
+            }
+        }
+        spell1 = count1 / 2;
+        cout << n - spell1 << endl;
+    }
+    return 0;
+}
