f9b5428a783c0d235abb8ffa9b48cb59917e9357
Hasnat Shafin <sh.shafin20@gmail.com>
2023-05-14 22:21:35 +0600

    initial commit

--- B-Spam.cpp
+++ B-Spam.cpp
@@ -1,17 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-        int x,y;
-        cin>>x>>y;
-        if(x>10*y){cout<<"YES"<<endl;}
-        else {cout<<"NO"<<endl;}
+        int x, y;
+        cin >> x >> y;
+        if (x > 10 * y)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }
---
+++ C-IQ.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int x;
+    cin >> x;
+    if (x + 7 > 170)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
