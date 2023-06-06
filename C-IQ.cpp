133e1287b3c3350886f6bebba6db2683614f71de
Hasnat Shafin <sh.shafin20@gmail.com>
2023-06-06 21:20:18 +0600

    Update Commit

--- C-IQ.cpp
+++ C-IQ.cpp
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
-using namespace std;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
     int x;
-    cin >> x;
+    std::cin >> x;
     if (x + 7 > 170)
-        cout << "YES";
+        std::cout << "YES";
     else
-        cout << "NO";
+        std::cout << "NO";
     return 0;
 }
