e49e8e4eb1f68fd4201d65ca3b13d68163be2952
Hasnat Shafin <sh.shafin20@gmail.com>
2023-06-17 14:47:41 +0600

    Initial commit though not completed

---
+++ C-PerfectArray.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define endl '\n';
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+int main()
+{
+    optimize();
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        
+    }
+    return 0;
+}
