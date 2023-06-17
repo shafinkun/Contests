9dd4bd3bed439dc6b19f3370c0c10564e4fe6ad8
Hasnat Shafin <sh.shafin20@gmail.com>
2023-06-18 00:26:54 +0600

    Accepted Code

--- C-PerfectArray.cpp
+++ C-PerfectArray.cpp
@@ -8,6 +8,26 @@ using namespace std;
 int main()
 {
     optimize();
-    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector <int> v;
+        for(int i=1;i<=n;i++)
+        {
+            v.push_back(i);
+        }
+        int sum=accumulate(v.begin(),v.end(),0);
+            
+        while(sum%n!=0){
+            v[0]+=1;
+            sum=accumulate(v.begin(),v.end(),0);
+        }
+        for(auto u:v)cout<<u<<" ";
+        cout<<endl;
+        
+    }
     return 0;
 }
