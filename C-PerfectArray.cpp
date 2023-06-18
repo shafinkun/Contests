22bf78c0d7c4a2fa53338bfaa77b32e406e06a4c
Hasnat Shafin <sh.shafin20@gmail.com>
2023-06-18 21:12:45 +0600

    Updated Code

--- C-PerfectArray.cpp
+++ C-PerfectArray.cpp
@@ -10,24 +10,25 @@ int main()
     optimize();
     int t;
     cin >> t;
-    while(t--)
+    while (t--)
     {
         int n;
-        cin>>n;
-        vector <int> v;
-        for(int i=1;i<=n;i++)
+        cin >> n;
+        vector<int> v;
+        for (int i = 1; i <= n; i++)
         {
             v.push_back(i);
         }
-        int sum=accumulate(v.begin(),v.end(),0);
-            
-        while(sum%n!=0){
-            v[0]+=1;
-            sum=accumulate(v.begin(),v.end(),0);
+        int sum = accumulate(v.begin(), v.end(), 0);
+
+        while (sum % n != 0)
+        {
+            v[0] += 1;
+            sum = accumulate(v.begin(), v.end(), 0);
         }
-        for(auto u:v)cout<<u<<" ";
-        cout<<endl;
-        
+        for (auto u : v)
+            cout << u << " ";
+        cout << endl;
     }
     return 0;
 }
