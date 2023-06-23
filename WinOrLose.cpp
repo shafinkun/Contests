90c5551478a55cbe560fdd69ae5ae26e42f97551
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2023-06-24 05:25:47 +0600

    Update WinOrLose.cpp

--- WinOrLose.cpp
+++ WinOrLose.cpp
@@ -1,4 +1,8 @@
 #include<bits/stdc++.h>
+#define endl '\n';
+#define  ios_base::sync_with_stdio(false); /
+        cin.tie(NULL); /
+        cout.tie(NULL);
 using namespace std;
 int main()
 {
@@ -7,8 +11,8 @@ int main()
     while(t--)
     {
         cin>>req>>over;
-        if(req<=(over*36)) cout<<"YES\n";    
-        else cout<<"NO\n";    
+        if(req<=(over*36)){ cout<<"YES"<<endl; }    
+        else {cout<<"NO"<<endl;}  
     }
     return 0;
 }
