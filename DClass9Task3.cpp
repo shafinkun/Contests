19853e1ba6c3b005c137b19284b08c9e7076b3f2
Hasnat Shafin <sh.shafin20@gmail.com>
2023-07-06 11:11:52 +0600

    Sumitted code

--- DClass9Task3.cpp
+++ DClass9Task3.cpp
@@ -4,18 +4,18 @@
     ios_base::sync_with_stdio(0); \
     cin.tie(0);                   \
     cout.tie(0);
-#define pi 3.14159265358979
+#define pi 3.141592653589793238
 
 using namespace std;
 int main()
 {
     optimize();
 
-    int r;
+    double r;
     cin >> r;
 
     double area = pi*r*r;
-    double cir = 2*pi*r;
+    double cir = 2.0*pi*r;
 
     cout << setprecision(10) << fixed << area <<" "<< setprecision(10) << fixed << cir << endl;
     
